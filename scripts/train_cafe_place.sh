3f746e60b6a0ab71dac295ae67427531569fbf56
cherixi_ClusterMaster <xiai.chn@outlook.com>
2025-11-22 11:29:30 +0800

    ci: --split place --drop_rate 0.15 --gar_nheads 8 --device "0, 1"

--- scripts/train_cafe_place.sh
+++ scripts/train_cafe_place.sh
@@ -1,3 +1,4 @@
 python train.py --data_path Dataset/ --split 'place'
 python train.py --data_path D:\\Cafe_Dataset\\Cafe_Dataset\\Dataset --split place --batch 4 --device "0"
 python train.py --data_path /share/share/aixi/Cafe_Dataset/Cafe_Dataset/Cafe_Dataset/Dataset/ --split place
+python train.py --data_path /share/share/aixi/Cafe_Dataset/Cafe_Dataset/Cafe_Dataset/Dataset/ --split place --drop_rate 0.15 --gar_nheads 8 --device "0, 1"   
